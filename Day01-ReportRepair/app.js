587b42895959586c8d774e112ab6fe5946771e9e
Cnomesta <36128569+Cnomesta@users.noreply.github.com>
2021-12-15 22:10:11 +0200

    AoC 2020 - Day 1 - Report Repair
    Added clock

--- Day01-ReportRepair/app.js
+++ Day01-ReportRepair/app.js
@@ -17,7 +17,8 @@ function part1() {
             let second = report[j];
             if (first + second === 2020) {
                 output = first * second;
-                console.log("PART-1:", "This,", first, "and this,", second, "is this:", output)
+                //console.log("PART-1:", "This,", first, "and this,", second, "is this:", output);
+                return [first, second, output];
             }
         }
         if (output != 0) {
@@ -26,7 +27,13 @@ function part1() {
     }
 }
 
-part1();
+(() => {
+    const begin = new Date();
+    const output = part1();
+    console.log("PART-1:", "Two numbers that sum to 2020 and their multiplication:", output);
+    const end = new Date() - begin;
+    console.info('Execution time: %dms', end);
+})();
 
 function part2() {
     let report = array;
@@ -39,7 +46,8 @@ function part2() {
                 let third = report[k];
                 if (first + second + third === 2020) {
                     output = first * second * third;
-                    console.log("PART-2:", "This,", first, "this,", second, "and this", third, "is this:", output);
+                    //console.log("PART-2:", "This,", first, "this,", second, "and this", third, "is this:", output);
+                    return [first, second, third, output];
                 }
             }
             if (output != 0) {
@@ -52,4 +60,10 @@ function part2() {
     }
 }
 
-part2();
+(() => {
+    const begin = new Date();
+    const output = part2();
+    console.log("PART-2:", "Three numbers that sum to 2020 and their multiplication:", output);
+    const end = new Date() - begin;
+    console.info('Execution time: %dms', end);
+})();
